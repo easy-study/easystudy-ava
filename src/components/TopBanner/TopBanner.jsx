6d6db5d906f4aa74c6ba755662b70a284debc06f
wenderinacio <eduinacius@gmail.com>
2022-06-23 19:16:14 -0300

    V1.5

--- src/components/TopBanner/TopBanner.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./TopBanner.css";
-
-export default function TopBanner() {
-  return (
-    <h3 className="notification-title">Você receberá avisos importantes aqui.</h3>
-  )
-}  
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SideMenu from './components/SideMenu/SideMenu';
 import TopBar from './components/TopBar/TopBar';
-// import TopBanner from './components/TopBanner/TopBanner';
 import MyCourses from './components/MyCourses/MyCourses';
 import './index.css';
+import './responsive.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +13,6 @@ root.render(
       <SideMenu />
       <div className='main'>
         <TopBar />
-        {/* <TopBanner /> */}
         <MyCourses />
       </div>
     </div>
