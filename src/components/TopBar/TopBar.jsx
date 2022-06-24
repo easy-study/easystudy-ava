3c21c82822082652708b15def15ce3061e9cfa73
wenderinacio <eduinacius@gmail.com>
2022-06-23 22:38:12 -0300

    V1.5

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -3,10 +3,16 @@ import "./TopBar.css";
 import {menu, user} from "../../assets/icons/icons";
 
 export default function TopBar() {
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  }
+
   return (
     <>
       <div className="topbar">
-      <div className="menu-btn"><img src={menu} alt="menu" /></div>
+      <div onClick={handleClick} className="menu-btn"><img src={menu} alt="menu" /></div>
           <h1 className="user-welcome">Bem-vindo de volta, User!</h1>
           <img className="user-image" src={user} alt="imagem do usuário"></img>
       </div>
