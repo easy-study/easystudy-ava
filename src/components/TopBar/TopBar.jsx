b27d50e4072ecd9e70c2d67f23e91a7e2dc20552
wenderinacio <eduinacius@gmail.com>
2022-06-25 21:20:04 -0300

    V1.5

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -4,17 +4,19 @@ import {menu, user} from "../../assets/icons/icons";
 
 export default function TopBar() {
 
+  const [menuIsOpen, setMenuIsOpen] = React.useState(false);
+
   const handleClick = (e) => {
     e.preventDefault();
-    console.log("clicked");
+    setMenuIsOpen(!menuIsOpen);
   }
 
   return (
     <>
       <div className="topbar">
-      <div onClick={handleClick} className="menu-btn"><img src={menu} alt="menu" /></div>
-          <h1 className="user-welcome">Bem-vindo de volta, User!</h1>
-          <img className="user-image" src={user} alt="imagem do usuário"></img>
+      <div onClick={handleClick} menuIsOpen={menuIsOpen} setMenuIsOpen={menuIsOpen ? 'show-menu' : ''} className="menu-btn"><img src={menu} alt="menu" /></div>
+        <h1 className="user-welcome">Bem-vindo de volta, User!</h1>
+        <img className="user-image" src={user} alt="imagem do usuário"></img>
       </div>
     </>
   )
