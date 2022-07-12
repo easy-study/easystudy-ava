435f88d6eeb92060355122a950617555cacd6b25
wenderinacio <eduinacius@gmail.com>
2022-07-12 19:11:49 -0300

    V1.6

--- src/components/MyCourses/MyCourses.jsx
+++ src/components/MyCourses/MyCourses.jsx
@@ -6,10 +6,9 @@ export default function MyCourses() {
     <div className="my-courses">
       <h2 className="section-title">Seus cursos em andamento</h2>
       <div className="carousel">
-        <div className="carousel-card"></div>
-        <div className="carousel-card"></div>
-        <div className="carousel-card"></div>
-        <div className="carousel-card"></div>
+        <div className="carousel-card">
+          <h3>Web Design</h3>
+        </div>
       </div>
     </div>
   )
--- src/components/RecommendedCourses/RecommendedCourses.jsx
+++ src/components/RecommendedCourses/RecommendedCourses.jsx
@@ -6,10 +6,10 @@ export default function RecommendedCourses() {
     <div className="recommended-courses">
       <h2 className="section-title">Cursos que podem ser do seu interesse</h2>
       <div className="carousel">
-        <div className="carousel-card"></div>
-        <div className="carousel-card"></div>
-        <div className="carousel-card"></div>
-        <div className="carousel-card"></div>
+        <div className="carousel-card">
+          <h3>React</h3>
+          <span>R$139.80</span>
+        </div>
       </div>
     </div>
   )
